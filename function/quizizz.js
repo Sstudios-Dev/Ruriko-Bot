4bf1c9c388c166be81e0904a1903b9e1ec68b12a
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-03 17:38:31 -0600

    commands

---
+++ function/quizizz.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-async-promise-executor */
+const fetch = require('node-fetch')
+
+/**
+ * @typedef QuizizzHack
+ * @property {string} answers
+ */
+
+/**
+ * Quizizz Hack Tools.
+ * @param {string} pin
+ * @returns {Promise<QuizizzHack>} answers
+ */
+const quizizz = (pin) => new Promise(async (resolve, reject) => {
+	if (!pin) return reject('Require Pin Query.')
+	const Data = await fetch('https://piyo.my.id/hack', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ pin: pin })
+	})
+	const Response = await Data.json()
+	if (Response.status === 400) return reject(Response.message)
+	resolve(Response)
+})
+
+module.exports = {
+  quizizz
+}

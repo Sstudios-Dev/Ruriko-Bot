5a6a0d3eca2f62265a1fda93727206d88912b22c
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-04 08:32:38 -0600

    Create test.ts

---
+++ function/test.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs-extra';
+
+
+/**
+ * Add AFK user.
+ * @param {string} userId
+ * @param {string} time
+ * @param {string} reason
+ * @param {AfkUser[]} _dir
+ */
+
+/**
+ * Print Hello World.
+ */
+const sayHelloWorld = () => {
+    console.log('Hello World!');
+}
+
+export {
+    sayHelloWorld // Add the new function to the exports
+}
